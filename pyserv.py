3ef3dd6cd1a19843cfca1ff46485f8f5a91828d2
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-01-07 10:52:37 +0100

    fixed
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- pyserv.py
+++ pyserv.py
@@ -214,12 +214,12 @@ class Services:
 
 	def flag(self, target):
 		for data in self.db.execute("select user from temp_nick where nick = '%s'" % target):
-			for flag in self.db.execute("select flag from channel where user = '%s'" % str(data[0])):
+			for flag in self.db.execute("select flag,channel from channel where user = '%s'" % str(data[0])):
 				if str(flag[0]) == "n":
-					self.mode(data.split()[2], "+q %s" % target)
+					self.mode(str(flag[1]), "+q %s" % target)
 				elif str(flag[0]) == "Y":
 					pass
 				else:
-					self.mode(data.split()[2], "+%s %s" % (str(flag[0]), target))
+					self.mode(str(flag[1]), "+%s %s" % (str(flag[0]), target))
 
 Services().run()
