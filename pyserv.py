b85a983ba8341e377d56cdd6c3711c88b2440a0a
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-04 20:48:41 +0100

    [66] oops
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- pyserv.py
+++ pyserv.py
@@ -383,40 +383,43 @@ class Services:
 				self.msg(source, "\2FEEDBACK\2 \37TEXT\37")
 		elif arg[0].lower() == "whois" and self.auth(source) != 0:
 			entry = False
-			if arg[1].startswith("#"):
-				for user in self.query("select name,email from users where name = '{0}'".format(arg[1][1:])):
-					entry = True
-					self.msg(source, "-Information for account {0}:".format(user[0]))
-					online = list()
-					for uid in self.query("select nick from temp_nick where user = '{0}'".format(user[0])):
-						for data in self.query("select nick from online where uid = '{0}'".format(uid[0])):
-							online.append(data[0])
-					self.msg(source, "Online Nicks: {0}".format(' '.join(online)))
-					self.msg(source, "Email address: {0}".format(user[1]))
-					self.msg(source, "Known on following channels:")
-					self.msg(source, "Channel              Flag")
-					for channel in self.query("select channel,flag from channels where user = '{0}'".format(user[0])):
-						self.msg(source, " {0}{1}{2}".format(channel[0], " "*int(20-len(channel[0])), channel[1]))
-					self.msg(source, "End of list.")
-			else:
-				for data in self.query("select uid from online where nick = '{0}'".format(arg[1])):
-					entry = True
-					for user in self.query("select user from temp_nick where nick = '{0}'".format(data[0])):
-						for account in self.query("select email from users where name = '{0}'".format(user[0])):
-							self.msg(source, "-Information for account {0}:".format(user[0]))
-							online = list()
-							for uid in self.query("select nick from temp_nick where user = '{0}'".format(user[0])):
-								for online_data in self.query("select nick from online where uid = '{0}'".format(uid[0])):
-									online.append(online_data[0])
-							self.msg(source, "Online Nicks: {0}".format(' '.join(online)))
-							self.msg(source, "Email address: {0}".format(account[0]))
-							self.msg(source, "Known on following channels:")
-							self.msg(source, "Channel              Flag")
+			if len(arg) == 1:
+				if arg[1].startswith("#"):
+					for user in self.query("select name,email from users where name = '{0}'".format(arg[1][1:])):
+						entry = True
+						self.msg(source, "-Information for account {0}:".format(user[0]))
+						online = list()
+						for uid in self.query("select nick from temp_nick where user = '{0}'".format(user[0])):
+							for data in self.query("select nick from online where uid = '{0}'".format(uid[0])):
+								online.append(data[0])
+						self.msg(source, "Online Nicks: {0}".format(' '.join(online)))
+						self.msg(source, "Email address: {0}".format(user[1]))
+						self.msg(source, "Known on following channels:")
+						self.msg(source, "Channel              Flag")
 						for channel in self.query("select channel,flag from channels where user = '{0}'".format(user[0])):
 							self.msg(source, " {0}{1}{2}".format(channel[0], " "*int(20-len(channel[0])), channel[1]))
 						self.msg(source, "End of list.")
-			if not entry:
-				self.msg(source, "Can\'t find user {0}".format(arg[1]))
+				else:
+					for data in self.query("select uid from online where nick = '{0}'".format(arg[1])):
+						entry = True
+						for user in self.query("select user from temp_nick where nick = '{0}'".format(data[0])):
+							for account in self.query("select email from users where name = '{0}'".format(user[0])):
+								self.msg(source, "-Information for account {0}:".format(user[0]))
+								online = list()
+								for uid in self.query("select nick from temp_nick where user = '{0}'".format(user[0])):
+									for online_data in self.query("select nick from online where uid = '{0}'".format(uid[0])):
+										online.append(online_data[0])
+								self.msg(source, "Online Nicks: {0}".format(' '.join(online)))
+								self.msg(source, "Email address: {0}".format(account[0]))
+								self.msg(source, "Known on following channels:")
+								self.msg(source, "Channel              Flag")
+							for channel in self.query("select channel,flag from channels where user = '{0}'".format(user[0])):
+								self.msg(source, " {0}{1}{2}".format(channel[0], " "*int(20-len(channel[0])), channel[1]))
+							self.msg(source, "End of list.")
+				if not entry:
+					self.msg(source, "Can\'t find user {0}".format(arg[1]))
+			else:
+				self.msg(source, "Syntax: \2WHOIS\2 \37(#)USER\37")
 		elif arg[0].lower() == "version": self.version(self.bot, source)
 		else:
 			self.msg(source, "Unknown command {0}. Please try HELP for more information.".format(arg[0].upper()))
