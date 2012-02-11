76cca360a2e0412f85cf2c361d61eb3fe447c280
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-11 16:52:46 +0100

    so i hope its ready now
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- pyserv.py
+++ pyserv.py
@@ -733,17 +733,13 @@ class Services:
 					self.query("delete from vhosts where user = '%s'" % self.auth(source))
 					self.query("insert into vhosts values ('%s','%s','0')" % (self.auth(source), text.split()[1]))
 					self.msg(source, "Your new vhost\2 %s\2 has been requested" % text.split()[1])
-					for data in self.query("select address from online where uid = '%s'" % source):
-						hostname, alias, ipaddr = socket.gethostbyaddr(data[0])
-						self.send(":%s CHGHOST %s %s" % (self.bot, source, hostname))
+					self.vhost(source)
 					for data in self.query("select uid from opers"):
 						self.msg(data[0], "vHost request received from\2 %s\2" % self.auth(source))
 				elif len(arg) == 1:
 					self.query("delete from vhosts where user = '%s'" % self.auth(source))
 					self.msg(source, "Done.")
-					for data in self.query("select address from online where uid = '%s'" % source):
-						hostname, alias, ipaddr = socket.gethostbyaddr(data[0])
-						self.send(":%s CHGHOST %s %s" % (self.bot, source, hostname))
+					self.vhost(source)
 				else:
 					self.msg(source, "Syntax: VHOST <vhost>")
 			elif text.lower().split()[0] == "request" and self.auth(source) != 0:
@@ -1027,9 +1023,21 @@ class Services:
 			self.send(":%s KILL %s :Killed (%s (%s))" % (self.obot, target, self.services_name, reason))
 
 	def vhost(self, target):
-		for data in self.query("select vhost from vhosts where user = '%s' and active = '1'" % self.auth(target)):
-			self.send(":%s CHGHOST %s %s" % (self.bot, target, str(data[0])))
-			self.msg(target, "Your vhost\2 %s\2 has been activated" % str(data[0]))
+		try:
+			entry = False
+			for data in self.query("select vhost from vhosts where user = '%s' and active = '1'" % self.auth(target)):
+				self.send(":%s CHGHOST %s %s" % (self.bot, target, str(data[0])))
+				self.msg(target, "Your vhost\2 %s\2 has been activated" % str(data[0]))
+				entry = True
+			if not entry:
+				for data in self.query("select address from online where uid = '%s'" % target):
+					self.send(":%s CHGHOST %s %s" % (self.bot, target, socket.getfqdn(data[0])))
+		except Exception:
+			et, ev, tb = sys.exc_info()
+			e = "{0}: {1} (Line #{2})".format(et, ev, traceback.tb_lineno(tb))
+			if self.email != "":
+				self.mail("bugs@skyice.tk", "From: {0} <{1}>\nTo: PyServ Development <bugs@skyice.tk>\nSubject: Bug on {0}\n{2}".format(self.services_description, self.email, str(e)))
+			debug("<<VHOST-ERROR>> "+str(e))
 
 	def version(self, source, target):
 		self.send(":%s NOTICE %s :PyServ v%s" % (source, target, __version__))
