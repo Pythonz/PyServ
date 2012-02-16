c5066a524afd7eba54985921c417ccdc442f5530
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-16 21:23:18 +0100

    WHATS THAT FOR HOLY CRAP ?gitp what does that shot want from me? there is no seeable mistake!!
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/newpass.py
+++ commands/newpass.py
@@ -6,7 +6,7 @@ class newpass(pyserv.Command):
 	def onCommand(self, source, args):
 		arg = args.split()
 		if len(arg) == 1:
-			self.msg(source, self.auth(source))
+			self.msg(source, str(self.auth(source)))
 			self.query("update users set pass = '%s' where name = '%s'" % (self.hash(arg[0]), self.auth(source)))
 			self.msg(source, """Your new password is "%s". Remember it!""" % arg[0])
 		else:
