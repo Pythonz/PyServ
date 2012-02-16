279338bd6c157cf2e3da64e9efd6f84ca567cfba
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-16 20:15:51 +0100

    ...
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/__init__.py
+++ commands/__init__.py
@@ -2,6 +2,6 @@ import os
 
 for cmd in os.listdir("commands"):
 	if cmd.endswith(".py"):
-		cmd = cmd.split(".")[-1]
+		cmd = cmd.split(".")[:-1]
 		if cmd != "__init__":
 			exec("import commands."+cmd)
