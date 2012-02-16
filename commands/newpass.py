ef383a921f0f81e050d9080fc35a9264763dd3f0
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-16 21:17:53 +0100

    :(
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/newpass.py
+++ commands/newpass.py
@@ -3,7 +3,7 @@ import pyserv
 class newpass(pyserv.Command):
 	help = ""
 	auth = 1
-	def onCommand(self, source, args)
+	def onCommand(self, source, args):
 		arg = args.split()
 		if len(arg) == 1:
 			self.query("update users set pass = '%s' where name = '%s'" % (self.hash(arg[1]), self.auth(source)))
