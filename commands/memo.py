9a85fe00d41b581db3e9377ee815aad5edc3514c
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-16 20:26:07 +0100

    what does that shot want from me? there is no seeable mistake!
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/memo.py
+++ commands/memo.py
@@ -5,33 +5,31 @@ class memo(pyserv.Command):
 	auth = 1
 	def onCommand(self, source, args):
 		arg = args.split()
-		if self.auth(source):
-			if len(arg) > 2:
-				if arg[1].startswith("#"):
-					user = arg[0][1:]
-					sender = self.auth(source)
-					message = _mysql.escape_string(' '.join(arg[1:]))
-					entry = False
-					for data in self.query("select name from users where name = '%s'" % user):
-						user = data[0]
-						entry = True
-					if entry:
-						self.query("insert into memo values ('%s', '%s', '%s')" % (user, sender, message))
-						self.msg(source, "Done.")
-						self.memo(user)
-					else: self.msg(source, "Can't find user %s." % arg[0])
-				else:
-					user = self.auth(self.uid(arg[0]))
-					sender = self.auth(source)
-					message = _mysql.escape_string(' '.join(arg[1:]))
-					entry = False
-					for data in self.query("select name from users where name = '%s'" % user):
-						user = data[0]
-						entry = True
-					if entry:
-						self.query("insert into memo values ('%s', '%s', '%s')" % (user, sender, message))
-						self.msg(source, "Done.")
-						self.memo(user)
-					else: self.msg(source, "Can't find user %s." % arg[0])
-			else: self.msg(source, "Syntax: MEMO <user> <message>")
-		else: self.unknown(source)
+		if len(arg) > 2:
+			if arg[1].startswith("#"):
+				user = arg[0][1:]
+				sender = self.auth(source)
+				message = _mysql.escape_string(' '.join(arg[1:]))
+				entry = False
+				for data in self.query("select name from users where name = '%s'" % user):
+					user = data[0]
+					entry = True
+				if entry:
+					self.query("insert into memo values ('%s', '%s', '%s')" % (user, sender, message))
+					self.msg(source, "Done.")
+					self.memo(user)
+				else: self.msg(source, "Can't find user %s." % arg[0])
+			else:
+				user = self.auth(self.uid(arg[0]))
+				sender = self.auth(source)
+				message = _mysql.escape_string(' '.join(arg[1:]))
+				entry = False
+				for data in self.query("select name from users where name = '%s'" % user):
+					user = data[0]
+					entry = True
+				if entry:
+					self.query("insert into memo values ('%s', '%s', '%s')" % (user, sender, message))
+					self.msg(source, "Done.")
+					self.memo(user)
+				else: self.msg(source, "Can't find user %s." % arg[0])
+		else: self.msg(source, "Syntax: MEMO <user> <message>")
--- commands/setwhois.py
+++ commands/setwhois.py
@@ -5,8 +5,8 @@ class setwhois(pyserv.Command):
 	auth = 1
 	def onCommand(self, source, args):
 		arg = args.split()
-		if len(arg) > 1:
-			self.send(":{uid} SWHOIS {target} :{text}".format(uid=self.bot, target=source, text=' '.join(arg[1:])))
+		if len(arg) > 0:
+			self.send(":{uid} SWHOIS {target} :{text}".format(uid=self.bot, target=source, text=' '.join(arg[0:])))
 			self.msg(source, "Done.")
 		else:
 			self.send(":{uid} SWHOIS {target} :".format(uid=self.bot, target=source))
