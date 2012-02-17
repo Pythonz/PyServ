acf0c7299eb2fc6853dd8f017b496222146a894e
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-17 22:08:19 +0100

    updated suspended
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/suspend.py
+++ commands/suspend.py
@@ -12,6 +12,7 @@ class suspend(pyserv.Command):
 					self.query("delete from suspended where channel = '%s'" % arg[0])
 					self.msg(uid, "Unsuspended.")
 				else: self.msg(uid, arg[0]+" is not suspended.")
+			elif arg[0] == "?": self.msg(uid, "Syntax: SUSPEND <#channel> [<reason>]")
 			else: self.msg(uid, "Invalid channel: "+arg[0])
 		elif len(arg) > 1:
 			if arg[0].startswith("#"):
@@ -27,4 +28,6 @@ class suspend(pyserv.Command):
 							self.gline(user, "Suspended "+arg[1]+": "+' '.join(arg[1]))
 				self.msg(uid, "Suspended.")
 			else: self.msg(uid, "Invalid channel: "+arg[0])
-		else: self.msg(uid, "Syntax: SUSPEND <#channel> [<reason>]")
+		else:
+			for data in self.query("select * from suspended"):
+				self.msg(uid, "Channel: {0} {1} Reason: {2}".format(data[0], " "*int(23-len(data[0])), data[1]))
