e90ca48d217230594c4b8faa1ca43f2e6f88a39b
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-17 16:42:49 +0100

    .
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/userlist.py
+++ commands/userlist.py
@@ -11,7 +11,7 @@ class userlist(pyserv.Command):
 					self.msg(uid, "Userlist for "+arg[0]+":")
 					for user in self.userlist(arg[0]):
 						self.msg(uid, "  "+user)
-					self.msg(uid, "End of list."
+					self.msg(uid, "End of list.")
 				else: self.msg(uid, "Channel "+arg[0]+" is not known.")
 			else: self.msg(uid, "Invalid channel: "+arg[0])
 		else: self.msg(uid, "Syntax: USERLIST <#channel>")
