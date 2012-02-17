5d27045a255d99b5471d050bf71a219a5520990f
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-17 16:15:38 +0100

    maybe now
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/banlist.py
+++ commands/banlist.py
@@ -1,5 +1,6 @@
 import pyserv
 
 class banlist(pyserv.Command):
+	help = "\2not ready for use\2"
 	nauth = 1
 	def onCommand(self, uid, args): pass
--- commands/unbanme.py
+++ commands/unbanme.py
@@ -1,5 +1,6 @@
 import pyserv
 
 class unbanme(pyserv.Command):
+	help = "\2not ready for use\2"
 	nauth = 1
 	def onCommand(self, uid, args): pass
