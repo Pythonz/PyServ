c14a598b90ced232d8f056dc2de13d2fc794cca8
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-17 18:17:54 +0100

    added new unbanall command
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

---
+++ commands/unbanall.py
@@ -0,0 +1,17 @@
+import pyserv
+
+class unbanall(pyserv.Command):
+	help = "Removes all bans from your channel"
+	nauth = 1
+	def onCommand(self, uid, args):
+		arg = args.split()
+		if len(arg) == 1:
+			if self.chanexist(arg[0]):
+				flag = self.getflag(uid, arg[0])
+				if flag == "n" or flag == "q" or flag == "a":
+					for ban in self.query("select ban from banlist where channel = '%s'" % arg[0]):
+						self.mode(arg[0], "-b "+ban[0])
+					self.msg(uid, "Done.")
+				else: self.msg(uid, "Denied.")
+			else: self.msg(uid, "Invalid channel: "+arg[0])
+		else: self.msg(uid, "Syntax: UNBANALL <#channel>")
