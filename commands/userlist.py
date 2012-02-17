b21311d891876f4f2213546fb1841f4a3870b06e
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-17 16:41:35 +0100

    so ...
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

---
+++ commands/userlist.py
@@ -0,0 +1,17 @@
+import pyserv
+
+class userlist(pyserv.Command):
+	help = "Shows you a list of users in that channel"
+	oper = 1
+	def onCommand(self, uid, args):
+		arg = args.split()
+		if len(arg) == 1:
+			if arg[0].startswith("#"):
+				if self.chanexist(arg[0]):
+					self.msg(uid, "Userlist for "+arg[0]+":")
+					for user in self.userlist(arg[0]):
+						self.msg(uid, "  "+user)
+					self.msg(uid, "End of list."
+				else: self.msg(uid, "Channel "+arg[0]+" is not known.")
+			else: self.msg(uid, "Invalid channel: "+arg[0])
+		else: self.msg(uid, "Syntax: USERLIST <#channel>")
