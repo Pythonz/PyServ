a2db40cf24b6cf725ca57210c734341563d10bee
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-17 22:45:38 +0100

    updated userlist
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/userlist.py
+++ commands/userlist.py
@@ -10,7 +10,7 @@ class userlist(pyserv.Command):
 				if self.chanexist(arg[0]):
 					self.msg(uid, "Userlist for "+arg[0]+":")
 					for user in self.userlist(arg[0]):
-						self.msg(uid, "  "+self.nick(user))
+						self.msg(uid, "  "+self.nick(user)+" ("+self.hostmask(user)+")")
 					self.msg(uid, "End of list.")
 				else: self.msg(uid, "Channel "+arg[0]+" is not known.")
 			else: self.msg(uid, "Invalid channel: "+arg[0])
