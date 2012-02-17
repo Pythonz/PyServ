67a31e3551157a53e05ed8d2002cd465b3ba9d09
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-17 22:11:17 +0100

    ... ready suspended
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- pyserv.py
+++ pyserv.py
@@ -221,8 +221,11 @@ class Services:
 								if pnick.find(",") != -1:
 									pnick = pnick.split(",")[1]
 								self.query("insert into chanlist value ('{0}','{1}')".format(pnick, fjoin_chan))
-								if self.suspended(fjoin_chan) and not self.isoper(pnick):
-									self.gline(pnick, "Suspended "+fjoin_chan+": "+self.suspended(fjoin_chan))
+								if self.suspended(fjoin_chan):
+									if not self.isoper(pnick):
+										self.gline(pnick, "Suspended "+fjoin_chan+": "+self.suspended(fjoin_chan))
+									else:
+										self.msg(pnick, "Channel "+fjoin_chan+" is suspended: "+self.suspend(fjoin_chan))
 							if self.chanexist(fjoin_chan): self.enforcebans(fjoin_chan)
 							if self.chanflag("l", fjoin_chan):
 								self.showlog(fjoin_nick, fjoin_chan)
