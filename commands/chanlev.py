963bf7eff7c0137c9bd9bd17c6e324294d93ad9c
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-18 13:03:53 +0100

    update
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/chanlev.py
+++ commands/chanlev.py
@@ -56,7 +56,7 @@ class chanlev(pyserv.Command):
 						for data in self.query("select name from users where name = '%s'" % username):
 							if str(self.auth(source)).lower() != username.lower():
 								self.query("delete from channels where channel = '%s' and user = '%s'" % (channel, username))
-								self.query("insert into channels values ('%s','%s','%s')" % (channel, username, arg[2]))
+								self.query("insert into channels values ('%s','%s','%s')" % (channel, username, arg[2][0]))
 								if self.sid(username) != 0:
 									for data in self.query("select nick from temp_nick where user = '%s'" % username):
 										self.flag(data[0])
@@ -70,7 +70,7 @@ class chanlev(pyserv.Command):
 							for data in self.query("select name from users where name = '%s'" % username):
 								if str(self.auth(source)).lower() != username.lower():
 									self.query("delete from channels where channel = '%s' and user = '%s'" % (channel, username))
-									self.query("insert into channels values ('%s','%s','%s')" % (channel, username, arg[2]))
+									self.query("insert into channels values ('%s','%s','%s')" % (channel, username, arg[2][0]))
 									if self.sid(username) != 0:
 										for data in self.query("select nick from temp_nick where user = '%s'" % username):
 											self.flag(data[0])
