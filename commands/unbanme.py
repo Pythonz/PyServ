485c9f9e5a6fc16bb9e8234b32934eacf8911d09
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-18 21:50:56 +0100

    what the hell is that
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/unbanme.py
+++ commands/unbanme.py
@@ -11,7 +11,7 @@ class unbanme(pyserv.Command):
 				flag = self.getflag(uid, arg[0])
 				if flag == "n" or flag == "q" or flag == "a" or flag == "o" or flag == "h":
 					for ban in self.query("select ban from banlist where channel = '%s'" % arg[0]):
-						if fnmatch(self.hostmask(uid), str(ban[0])):
+						if fnmatch(str(self.hostmask(uid)), str(ban[0])):
 							self.mode(arg[0], "-b "+ban[0])
 							self.query("delete from banlist where channel = '%s' and ban = '%s'" % (arg[0], ban[0]))
 					self.msg(uid, "Done.")
