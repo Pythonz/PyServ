68a82e404da107180dceb40399d6fcdbfd1b7e50
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-18 18:12:36 +0100

    updated howto file
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env
+#!/usr/bin/env python
 
 from subprocess import Popen as shell
 
 shell("python setup.py install", shell=True, cwd="deps/mysql").wait()
-shell("python setup.py build", shell=True, cwd="deps/pywhois").wait()
+shell("python setup.py install", shell=True, cwd="deps/pywhois").wait()
