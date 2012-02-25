953ac84b04c4100a69755f3e7af60811f5785c94
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-25 19:06:15 +0100

    fixed challenge
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/challenge.py
+++ commands/challenge.py
@@ -5,6 +5,6 @@ class challenge(Command):
 	def onCommand(self, uid, args):
 		if self.auth(uid) == 0:
 			import hmac
-			self.msg(uid, "CHALLENGE {0} HMAC-MD5 HMAC-SHA-1 HMAC-SHA-256 HMAC-SHA-512".format(hmac.new(self.hostmask(uid)).hexdigest()))
+			self.msg(uid, "CHALLENGE {0} HMAC-MD5 HMAC-SHA-1 HMAC-SHA-256 HMAC-SHA-512".format(hmac.new(self.hostmask(uid)[0]).hexdigest()))
 		else:
 			self.msg(uid, "CHALLENGE is not available once you have authed.")
