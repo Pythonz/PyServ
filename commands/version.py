21e31e27072495671501c3e1e24fd9f5771aafc2
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-25 11:51:20 +0100

    try that
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/version.py
+++ commands/version.py
@@ -20,9 +20,14 @@ class version(Command):
 			self.msg(source, "Options: {0}".format(', '.join(options)))
 		if self.isoper(source):
 			import psutil
-			self.msg(source, "Usage:")
+			self.msg(source, "CPU:")
 			i = 0
 			for cpu in psutil.cpu_percent(interval=0, percpu=True):
 				i += 1
 				self.msg(source, "  CPU_{0}: {1}%".format(i, cpu))
+			self.msg(source, "Memory:")
+			self.msg(source, "  Total: {0} mb".format(psutil.phymem_usage()[0] / 1024 / 1024))
+			self.msg(source, "  Free: {0} mb".format(psutil.phymem_usage()[1] / 1024 / 1024))
+			self.msg(source, "  Used: {0} mb".format(psutil.phymem_usage()[2] / 1024 / 1024))
+			
 		self.msg(source, "Developed by Pythonz (https://github.com/Pythonz). Suggestions to pythonz@skyice.tk.")
