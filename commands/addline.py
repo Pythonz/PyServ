727978799642e00d0a24f00f1640f611bdf80d8f
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-02-26 13:41:10 +0100

    forget it
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/addline.py
+++
@@ -1,9 +0,0 @@
-from pyserv import Command
-
-class addline(Command):
-	help = "TEST"
-	oper = 1
-	nauth = 1
-	def onCommand(self, uid, args):
-		self.send(":%s ADDLINE" % self.bot)
-		self.msg(uid, "Done.")
