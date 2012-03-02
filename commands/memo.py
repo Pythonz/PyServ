1de9acb8978f1b0b1bd464bc66e40f0379d745f7
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-02 22:19:49 +0100

    fixit
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/memo.py
+++ commands/memo.py
@@ -8,26 +8,18 @@ class memo(Command):
 		if len(arg) > 1:
 			if arg[0].startswith("#"):
 				user = arg[0][1:]
-				sender = self.auth(source)
-				message = _mysql.escape_string(' '.join(arg[1:]))
-				entry = False
-				for data in self.query("select name from users where name = '%s'" % user):
-					user = data["name"]
-					entry = True
-				if entry:
+				if self.user(user):
+					sender = self.auth(source)
+					message = _mysql.escape_string(' '.join(arg[1:]))
 					self.query("insert into memo values ('%s', '%s', '%s')" % (user, sender, message))
 					self.msg(source, "Done.")
 					self.memo(user)
 				else: self.msg(source, "Can't find user %s." % arg[0])
 			else:
 				user = self.auth(self.uid(arg[0]))
-				sender = self.auth(source)
-				message = _mysql.escape_string(' '.join(arg[1:]))
-				entry = False
-				for data in self.query("select name from users where name = '%s'" % user):
-					user = data["name"]
-					entry = True
-				if entry:
+				if self.user(user):
+					sender = self.auth(source)
+					message = _mysql.escape_string(' '.join(arg[1:]))
 					self.query("insert into memo values ('%s', '%s', '%s')" % (user, sender, message))
 					self.msg(source, "Done.")
 					self.memo(user)
