9d640c25876a5f160f8c3743410bca0b76fd9265
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-03 13:18:38 +0100

    update ipchan
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/ipchan.py
+++ commands/ipchan.py
@@ -6,13 +6,11 @@ class ipchan(Command):
 	def onCommand(self, uid, args):
 		arg = args.split()
 		if len(arg) == 0:
-			self.msg(uid, "IP forced channels:")
 			self.msg(uid, "IP                 Channel")
 			for data in self.query("select ip,channel from ipchan"):
 				self.msg(uid, "  {0} {1} {2}".format(data["ip"], " "*int(15-len(data["ip"])), data["channel"]))
 			self.msg(uid, "End of list.")
 		elif len(arg) == 1:
-			self.msg(uid, "IP forced channels:")
 			self.msg(uid, "IP                 Channel")
 			for data in self.query("select ip,channel from ipchan where channel = '%s'" % arg[0]):
 				self.msg(uid, "  {0} {1} {2}".format(data["ip"], " "*int(15-len(data["ip"])), data["channel"]))
