02485368602ad672fc4d1a1e41cd3ad54ec9243b
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-03 11:13:34 +0100

    added sanick and samode
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

---
+++ commands/samode.py
@@ -0,0 +1,18 @@
+from pyserv import Command
+
+class samode(Command):
+	help = "Change modes on a channel where you have no rights"
+	oper = 1
+	def onCommand(self, uid. args):
+		arg = args.split()
+		if len(arg) > 1:
+			if arg[0].startswith("#"):
+				self.mode(arg[0], ' '.join(arg[1:]))
+				self.msg(uid, "Done.")
+			else:
+				self.msg(uid, "Invalid channel: " + arg[0])
+		else:
+			self.msg(uid, "Syntax: SAMODE <#channel> <modes>")
+
+	def onFantasy(self, uid, chan, args):
+		self.onCommand(uid, chan + " " + args)
---
+++ commands/sanick.py
@@ -0,0 +1,16 @@
+from pyserv import Command
+
+class sanick(Command):
+	help = "Change nicks of others"
+	oper = 1
+	def onCommand(self, uid, args):
+		arg = args.split()
+		if len(arg) == 2:
+			target = self.uid(arg[0])
+			if target != arg[0] and target != self.bot:
+				self.send(":" + self.bot + " SVSNICK " + target + " " + arg[1])
+				self.msg(uid, "Done.")
+			else:
+				self.msg(uid, "Denied.")
+		else:
+			self.msg(uid, "Syntax: SANICK <nick> <newnick>")
