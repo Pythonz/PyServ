80cf560a40e9e2bcaa1afd0f3b52241a72288b4c
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-04 19:12:51 +0100

    update, add sakick
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/kick.py
+++ commands/kick.py
@@ -6,24 +6,30 @@ class kick(Command):
 	def onCommand(self, source, args):
 		arg = args.split()
 		if len(arg) == 2:
-			if arg[0].startswith("#"):
+			if self.chanexist(arg[0]):
 				flag = self.getflag(source, arg[0])
 				if flag == "n" or flag == "q" or flag == "a" or flag =="o"or flag =="h":
 					if arg[1].lower() != self.bot_nick.lower() and not self.isoper(self.uid(arg[1])):
 						if self.onchan(arg[0],arg[1]):
-							self.kick(arg[0], arg[1])
+							if self.chanflag("k", arg[0]):
+								self.kick(arg[0], arg[1], self.nick(source))
+							else:
+								self.kick(arg[0], arg[1])
 							self.msg(source, "Done.")
 						else: self.msg(source, arg[1]+" is not on channel "+arg[0])
 					else: self.msg(source, "Denied.")
 				else: self.msg(source, "Denied.")
 			else: self.msg(source, "Invalid channel")
 		elif len(arg) > 2:
-			if arg[0].startswith("#"):
+			if self.chanexist(arg[0]):
 				flag = self.getflag(source, arg[0])
 				if flag == "n" or flag == "q" or flag == "a" or flag =="o"or flag =="h":
 					if arg[1].lower() != self.bot_nick.lower() and not self.isoper(self.uid(arg[1])):
 						if self.onchan(arg[0],arg[1]):
-							self.kick(arg[0], arg[1], ' '.join(arg[2:]))
+							if self.chanflag("k", arg[0]):
+								self.kick(arg[0], arg[1], ' '.join(arg[2:]) + " (" + self.nick(source) + ")")
+							else:
+								self.kick(arg[0], arg[1], ' '.join(arg[2:]))
 							self.msg(source, "Done.")
 						else: self.msg(source, arg[1]+" is not on channel "+arg[0])
 					else: self.msg(source, "Denied.")
---
+++ commands/sakick.py
@@ -0,0 +1,29 @@
+from pyserv import Command
+
+class kick(Command):
+	help = "Kicks someone from the channel"
+	oper = 1
+	def onCommand(self, source, args):
+		arg = args.split()
+		if len(arg) == 2:
+			if arg[0].startswith("#"):
+				if arg[1].lower() != self.bot_nick.lower() and not self.isoper(self.uid(arg[1])):
+					if self.onchan(arg[0],arg[1]):
+						self.kick(arg[0], arg[1])
+						self.msg(source, "Done.")
+					else: self.msg(source, arg[1]+" is not on channel "+arg[0])
+				else: self.msg(source, "Denied.")
+			else: self.msg(source, "Invalid channel")
+		elif len(arg) > 2:
+			if arg[0].startswith("#"):
+				if arg[1].lower() != self.bot_nick.lower() and not self.isoper(self.uid(arg[1])):
+					if self.onchan(arg[0],arg[1]):
+						self.kick(arg[0], arg[1], ' '.join(arg[2:]))
+						self.msg(source, "Done.")
+					else: self.msg(source, arg[1]+" is not on channel "+arg[0])
+				else: self.msg(source, "Denied.")
+			else: self.msg(source, "Invalid channel")
+		else: self.msg(source, "Syntax: KICK <#channel> <user> [,<user>] [reason]")
+
+	def onFantasy(self, uid, chan, args):
+		self.onCommand(uid, chan + " " + args)
