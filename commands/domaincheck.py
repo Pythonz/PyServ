c474bcf9c9ba3faf93585d2d6080b9d946b5108f
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-10 13:20:25 +0100

    show website title (if website is available, to check for ads)
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/domaincheck.py
+++ commands/domaincheck.py
@@ -7,7 +7,14 @@ class domaincheck(Command):
 		arg = args.split()
 		if len(arg) == 1:
 			from pywhois import whois
+			from urllib2 import urlopen
 			self.msg(uid, "Check domain: "+arg[0])
+			if self.scanport(arg[0], 80) or self.scanport(arg[0], 443):
+				site = urlopen(arg[0])
+				content = site.read()
+				site.close()
+				title = content.split("<title>")[1].split("</title>")[0]
+				self.msg(uid, "Title: " + title)
 			domain = str(whois(arg[0])).splitlines()
 			for line in domain:
 				self.msg(uid, line)
