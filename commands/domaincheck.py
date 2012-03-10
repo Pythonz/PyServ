c817e2ddd08f424fe9aa153ed0b82b8a9c37a1be
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-10 16:19:21 +0100

    simply to sort it better
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/domaincheck.py
+++ commands/domaincheck.py
@@ -7,12 +7,16 @@ class domaincheck(Command):
 		arg = args.split()
 		if len(arg) == 1:
 			from subprocess import Popen, PIPE
+			self.msg(uid, ".-: Domain-Check :-.")
+			self.msg(uid)
 			domain = Popen(["whois", arg[0]], stdout=PIPE).stdout.read().splitlines()
 			for line in domain:
 				if line != "" and line[0] != "%":
 					if line[0] == "[" and line[-1] == "]":
 						self.msg(uid)
 					self.msg(uid, line)
+			self.msg(uid)
+			self.msg(uid, ".-: End of Domain-Check :-.")
 		else: self.msg(uid, "Syntax: DOMAINCHECK <domain>")
 
 	def onFantasy(self, uid, channel, args):
