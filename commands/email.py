adfc59837df6d52a7393fa88d78cb5ac746407bb
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-12 19:33:48 +0100

    now its better
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/email.py
+++ commands/email.py
@@ -15,7 +15,7 @@ class email(Command):
 								entry = True
 							if not entry:
 								self.query("update users set email = '%s' where name = '%s'" % (arg[0], self.auth(uid)))
-								self.mail(arg[0], "From: {server} <{servermail}>\nTo: {user} <{usermail}>\nSubject: {user}\nYour Email address has been changed to {usermail} successfully.".format(server=self.services_description, servermail=self.email, user=self.auth(uid), usermail=arg[0]))
+								self.mail(arg[0], "From: {server} <{servermail}>\nTo: {user} <{usermail}>\nSubject: Email verification\nYour Email address has been changed to {usermail} successfully.".format(server=self.services_description, servermail=self.email, user=self.auth(uid), usermail=arg[0]))
 								self.msg(uid, "Done.")
 							else: self.msg(uid, "Email address already in use.")
 						else: self.msg(uid, "Invalid email: "+arg[0])
