0ee6427e25a84e305b71dfc5a81a67e8c5fe4583
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-17 10:36:07 +0100

    MD5-Checksum: f0c1b54a4b5e9544133998379e2385aa
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/sahello.py
+++ commands/sahello.py
@@ -10,8 +10,8 @@ class sahello(Command):
 			for data in self.query("select name from users where name = '%s'" % arg[0]):
 				entry = True
 			if not entry:
-				self.query("insert into users values ('%s', '%s', '%s@%s', 'n', '+i', '0')" % (arg[0], self.encode(arg[1]), self.bot_nick, self.services_name))
 				self.msg(uid, "Create account (%s, %s) ..." % (arg[0], arg[1]))
+				self.query("insert into users values ('%s', '%s', '%s@%s', 'n', '+i', '0')" % (arg[0], self.encode(arg[1]), self.bot_nick, self.services_name))
 				self.msg(uid, "Done.")
 			else:
 				self.msg(uid, "%s is already in use." % arg[0])
