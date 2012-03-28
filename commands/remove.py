e384d1447c51e9eadc025bf29e45690f8404f65a
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-28 15:27:54 +0200

    [742] checksum: 4cb50380cc4ad6d4fc2ff815184e87b6
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/remove.py
+++ commands/remove.py
@@ -9,10 +9,10 @@ class remove(Command):
 			if arg[0].startswith("#"):
 				if self.getflag(source, arg[0]) == "n":
 					for data in self.query("select name from channelinfo where name = '{0}'".format(escape_string(arg[0]))):
-						self.query("delete from channels where channel = '{0}'".format(data["name"]))
-						self.query("delete from channelinfo where name = '{0}'".format(data["name"]))
-						self.msg(source, "Channel {0} has been deleted.".format(data["name"]))
-						self.send(":{0} PART {1} :Channel {1} has been deleted.".format(self.bot, data["name"]))
+						self.query("delete from channels where channel = '{0}'".format(escape_string(data["name"])))
+						self.query("delete from channelinfo where name = '{0}'".format(escape_string(data["name"])))
+						self.msg(source, "Channel {0} has been deleted.".format(escape_string(data["name"])))
+						self.send(":{0} PART {1} :Channel {1} has been deleted.".format(self.bot, escape_string(data["name"])))
 				else: self.msg(source, "No permission")
 			else: self.msg(source, "Invalid channel: {0}".format(arg[0]))
 		else: self.msg(source, "Syntax: REMOVE <#channel>")
