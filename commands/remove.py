7debad300bba29df622c41921648b00bec6c30fb
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-03-28 12:35:59 +0200

    [741] checksum: 3c38552047a8c3c1ce560a8ef25861be
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- commands/remove.py
+++ commands/remove.py
@@ -8,11 +8,11 @@ class remove(Command):
 		if len(arg) == 1:
 			if arg[0].startswith("#"):
 				if self.getflag(source, arg[0]) == "n":
-					for data in self.query("select name from channelinfo where name = '{0}'".format(arg[0])):
+					for data in self.query("select name from channelinfo where name = '{0}'".format(escape_string(arg[0]))):
 						self.query("delete from channels where channel = '{0}'".format(data["name"]))
 						self.query("delete from channelinfo where name = '{0}'".format(data["name"]))
 						self.msg(source, "Channel {0} has been deleted.".format(data["name"]))
 						self.send(":{0} PART {1} :Channel {1} has been deleted.".format(self.bot, data["name"]))
 				else: self.msg(source, "No permission")
-			else: self.msg(source, "Invalid channel".format(arg[0]))
+			else: self.msg(source, "Invalid channel: {0}".format(arg[0]))
 		else: self.msg(source, "Syntax: REMOVE <#channel>")
--- commands/request.py
+++ commands/request.py
@@ -1,4 +1,5 @@
 from pyserv import Command
+from _mysql import escape_string
 
 class request(Command):
 	help = "Requests Q for your channel"
@@ -8,12 +9,12 @@ class request(Command):
 		if len(arg) == 1:
 			if arg[0].startswith("#"):
 				exists = False
-				for data in self.query("select channel from channels where channel = '%s'" % arg[0]):
+				for data in self.query("select channel from channels where channel = '%s'" % escape_string(arg[0])):
 						exists = True
 				if not exists:
 					if not self.suspended(arg[0]):
-						self.query("insert into channelinfo values ('%s', '', '', '', '', '10:5', '!')" % arg[0])
-						self.query("insert into channels values ('%s','%s','n')" % (arg[0], self.auth(source)))
+						self.query("insert into channelinfo values ('%s', '', '', '', '', '10:5', '!')" % escape_string(arg[0]))
+						self.query("insert into channels values ('%s','%s','n')" % (escape_string(arg[0]), self.auth(source)))
 						self.join(arg[0])
 						self.mode(arg[0], "+q {0}".format(source))
 						self.msg(source, "Channel %s has been registered for you" % arg[0])
@@ -21,6 +22,6 @@ class request(Command):
 						self.msg(source, "Channel " + arg[0] + " is suspended: " + self.suspended(arg[0]))
 				else:
 					self.msg(source, "Channel %s is already registered" % arg[0])
-			else: self.msg(source, "Invalid channel")
+			else: self.msg(source, "Invalid channel: {0}".format(arg[0]))
 		else:
 			self.msg(source, "Syntax: REQUEST <#channel>")
