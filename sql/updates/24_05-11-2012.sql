b28c7280fc33ce726025a0877577dc9fa32907f9
Pascal Vahlberg <pascalvahlberg@yahoo.de>
2012-05-11 16:45:28 +0200

    [767] Other/Checksum: 3ec3966e6250f4dcd9ecf0920ddcc07f
    Signed-off-by: Pascal Vahlberg <pascalvahlberg@yahoo.de>

--- sql/updates/24_05-11-2012.sql
+++ sql/updates/24_05-11-2012.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `users` ADD COLUMN id bigint(20) FIRST;
-ALTER TABLE `users` MODIFY COLUMN (id bigint(20) not null auto_increment);
+ALTER TABLE `users` MODIFY COLUMN id bigint(20) not null auto_increment;
 ALTER TABLE `users` ADD PRIMARY KEY (id);
 DROP TABLE IF EXISTS `statistics`;
 CREATE TABLE IF NOT EXISTS `statistics` (`attribute` text not null, `value` text, primary key (`attribute`));
