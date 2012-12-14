613d18e51fa0e7c270e3011d2a3e01d6cbabec4f
Chiruclan <pascalvahlberg@yahoo.de>
2012-12-14 16:40:40 +0100

    [808] ...
    Signed-off-by: Chiruclan <pascalvahlberg@yahoo.de>

--- commands/hello.py
+++ commands/hello.py
@@ -23,13 +23,13 @@ class hello(Command):
 					if arg[0].find("@") != -1 and arg[0].find(".") != -1 and arg[0].lower() == arg[1].lower():
 						newpw = str(hash(str(time()) + arg[0] + arg[1]))
 						self.query("insert into users (name,pass,email,flags,modes,suspended) values ('%s','%s','%s','n','+i','0')" % (self.nick(source), self.encode(newpw), escape_string(arg[0])))
-						self.msg(source, "The account %s has been created successfully. You can login now with /msg Q auth account password" % self.nick(source))
+						self.msg(source, "The account %s has been created successfully. You can login now with /msg %s@%s auth account password" % (self.nick(source), self.bot_nick, self.services_name))
 						
 						if self.regmail == "1":
 							self.msg(source, "An email had been send to you with your password!")
-							self.mail(arg[0], """From: %s <%s>\nTo: %s <%s>\nSubject: Your account on %s\n\nWelcome to %s\nYour account data:\n\nUser: %s\nPassword: %s\n\nAuth via "/MSG Q@%s AUTH %s %s"\nChange your password as soon as possible with "/MSG Q%s NEWPASS NEWPASS"!""" % (self.services_description, self.email, self.nick(source), arg[0], self.services_description, self.services_description, self.nick(source), newpw, self.services_name, self.nick(source), newpw, self.services_name))
+							self.mail(arg[0], """From: {0} <{1}>\nTo: {2} <{3}>\nSubject: Your account on {4}\n\nWelcome to {4}\nYour account data:\n\nUser: {2}\nPassword: {5}\n\nAuth via "/MSG {6}@{7} AUTH {2} {5}"\nChange your password as soon as possible with "/MSG {6}@{7} NEWPASS <NEWPASSWORD>"!""".format(self.services_description, self.email, self.nick(source), arg[0], self.services_description, newpw, self.bot_nick, self.services_name))
 						else:
-							self.msg(source, """Use "/msg Q auth %s %s" to auth""" % (self.nick(source), newpw))
+							self.msg(source, """Use "/msg %s@%s auth %s %s" to auth""" % (self.bot_nick, self.services_name, self.nick(source), newpw))
 							self.msg(source, "Change your password as soon as possible!")
 					else:
 						self.msg(source, "Invalid email %s!" % arg[0])
