02ab4b99fe66add2d66ec9cac521cfe65419f8bf
Chiruclan <pascalvahlberg@yahoo.de>
2012-12-15 18:41:28 +0100

    [814] adding glines-table for coming features
    Signed-off-by: Chiruclan <pascalvahlberg@yahoo.de>

--- sql/base/struct.sql
+++ sql/base/struct.sql
@@ -107,6 +107,12 @@ CREATE TABLE IF NOT EXISTS `gateway` (
 	`uid` text
 );
 
+DROP TABLE IF EXISTS `glines`;
+CREATE TABLE IF NOT EXISTS `glines` (
+	`id` bigint(20) not null auto_increment key,
+	`mask` text
+);
+
 DROP TABLE IF EXISTS `statistics`;
 CREATE TABLE IF NOT EXISTS `statistics` (
 	`attribute` varchar(256) not null key,
---
+++ sql/updates/26_12-15-2012.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS `glines`;
+CREATE TABLE IF NOT EXISTS `glines` (`id` bigint(20) not null auto_increment key, `mask` text);
