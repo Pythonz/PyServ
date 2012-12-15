c90831bd07d3817503c470f842ac47bdf2469dd5
Chiruclan <pascalvahlberg@yahoo.de>
2012-12-15 19:12:37 +0100

    [819] Other/Checksum: 53c034145e6c4fe0eaf3476f3315e4dd
    Signed-off-by: Chiruclan <pascalvahlberg@yahoo.de>

--- commands/gline.py
+++ commands/gline.py
@@ -12,7 +12,7 @@ class gline(Command):
 					self.gline(arg[0])
 					self.msg(uid, "Done.")
 				else:
-					self.msg(uid, "You cannot g-line another operator!")
+					self.msg(uid, "You cannot g-line an irc operator!")
 			else:
 				self.msg(uid, "Cannot find user '" + arg[0] + "' on the network.")
 		elif len(arg) > 1:
@@ -21,7 +21,7 @@ class gline(Command):
 					self.gline(arg[0], ' '.join(arg[1:]))
 					self.msg(uid, "Done.")
 				else:
-					self.msg(uid, "You cannot g-line another operator!")
+					self.msg(uid, "You cannot g-line an irc operator!")
 			else:
 				self.msg(uid, "Cannot find user '" + arg[0] + "' on the network.")
 		else:
