2bcbd1370e16923b66e697393b8802eaff806d65
Chiruclan <pascalvahlberg@yahoo.de>
2012-12-18 17:44:49 +0100

    [857] damn
    Signed-off-by: Chiruclan <pascalvahlberg@yahoo.de>

--- commands/chanmode.py
+++ commands/chanmode.py
@@ -11,7 +11,7 @@ class chanmode(Command):
 		if len(arg) == 1:
 			if arg[0].startswith("#"):
 				if self.getflag(source, arg[0]) == "n" or self.getflag(source, arg[0]) == "q" or self.getflag(source, arg[0]) == "a":
-					for channel in self.query("select name,modes from channelinfo where name = '{0}'".format(excape_string(arg[0]))):
+					for channel in self.query("select name,modes from channelinfo where name = '{0}'".format(escape_string(arg[0]))):
 						self.msg(source, "Current modes for {0}: {1}".format(channel["name"], channel["modes"]))
 				else:
 					self.msg(source, "Denied.")
@@ -22,9 +22,9 @@ class chanmode(Command):
 			
 			if arg[0].startswith("#"):
 				if self.getflag(source, arg[0]) == "n" or self.getflag(source, arg[0]) == "q" or self.getflag(source, arg[0]) == "a":
-					for channel in self.query("select name from channelinfo where name = '{0}'".format(excape_string(arg[0]))):
+					for channel in self.query("select name from channelinfo where name = '{0}'".format(escape_string(arg[0]))):
 						modes = self.regexflag(channel["flags"], modes, True)
-						self.query("update channelinfo set modes = '{0}' where name = '{1}'".format(excape_string(modes), channel["name"]))
+						self.query("update channelinfo set modes = '{0}' where name = '{1}'".format(escape_string(modes), channel["name"]))
 						self.mode(channel["name"], modes)
 						self.msg(source, "Done. New modes for {0}: {1}".format(channel["name"], modes))
 				else:
