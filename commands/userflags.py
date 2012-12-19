58a9f4315410e51470c990c987ff0d779ca48536
Chiruclan <pascalvahlberg@yahoo.de>
2012-12-19 15:36:08 +0100

    [863] Other/Checksum: 7aaa9b8a57e46f6ae0913f3681853b9a
    Signed-off-by: Chiruclan <pascalvahlberg@yahoo.de>

--- commands/userflags.py
+++ commands/userflags.py
@@ -1,29 +1,37 @@
 from pyserv import Command
+from _mysql import escape_string
 
 class userflags(Command):
 	help = "Changes and shows your userflags"
 	nauth = 1
 
 	def onCommand(self, uid, args):
-		flags = list()
+		mode = list()
+		desc = list()
 		mode.append("n")
-		flags.append(["n", "{0} will answer with notice instead of query.".format(self.bot_nick)])
-		flags.append(["a", "Autojoin all channels where you got chanlev +v or higher."])
-		flags.append(["x", "Enable cloaked hosts"])
+		desc.append("%s will answer with notices, instead of privmsgs." % self.bot_nick)
+		mode.append("a")
+		desc.append("Autojoin all channels where you have chanflag +v or higher.")
+		mode.append("x")
+		desc.append("Cloak your hostname.")
 		arg = args.split()
 		
 		if len(arg) == 0:
 			self.msg(uid, "Current user flags: +"+self.userflags(uid))
 		elif len(arg) == 1:
 			if arg[0] == "?":
-				for flag in flags:
-					self.msg(uid, "+{0} = {1}".format(flag[0], flag[1]))
+				i = 0
+				
+				for flag in 
+				while i != len(mode):
+					self.msg(uid, "+" + mode[i]+" = "+desc[i])
+					i += 1
 			else:
 				userflags = self.regexflag("+" + self.userflags(uid), arg[0])
 				flags = ''.join([char for char in userflags if char in ''.join(mode)])
-				self.query("update users set flags = '%s' where name = '%s'" % (flags, self.auth(uid)))
-				self.msg(uid, "Done. Current user flags: +" + userflags)
 				if arg[0].find("x") != -1:
 					self.vhost(uid)
+				self.query("update users set flags = '%s' where name = '%s'" % (escape_string(flags), self.auth(uid)))
+				self.msg(uid, "Done. Current user flags: +" + flags)
 		else:
 			self.msg(uid, "Syntax: USERFLAGS [<flags>]")
